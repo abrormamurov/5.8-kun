5670165098694a2e67510262053e50cb4c093ab2
abrormamurov <mabrorbk@gmail.com>
2024-03-30 04:42:32 +0500

    added

---
+++ app.js
@@ -0,0 +1,27 @@
+// const btn = document.querySelectorAll(".btn");
+// let birinchibtn = null;
+
+// btn.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     if (birinchibtn) {
+//       if (birinchibtn !== btn && birinchibtn.textContent === btn.textContent) {
+//         btn.style.background = "red";
+//       } else {
+//         birinchibtn.style.background = "green";
+//       }
+//       birinchibtn = null;
+//     } else {
+//       btn.style.background = "red";
+//       birinchibtn = btn;
+//     }
+//   });
+// });
+
+function changeColor(clickedButton) {
+  var buttons = document.querySelectorAll(".button"); // Barcha tugmalarni tanlaymiz
+
+  // Har bir tugmani bir hil rang qilamiz
+  buttons.forEach(function (button) {
+    button.style.backgroundColor = "red";
+  });
+}
